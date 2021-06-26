fa239e16cb5d71808a2f3702a0af989002adeab3
Nishat009 <nishatraihana009@gmail.com>
2021-06-27 00:08:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from 'react';
+import './App.css';
+import Header from './Component/Shared/Header/Header';
+import Home from './Component/Home/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  
+} from "react-router-dom";
+import Login from './Component/Login/Login/Login';
+import PrivateRoute from './Component/Login/PrivateRoute/privateRoute';
+import Dashboard from './Component/Dashboard/Dashboard/Dashboard';
+import AddBlog from './Component/Dashboard/AddBlog/AddBlog';
+import ManageBlog from './Component/Dashboard/ManageBlog/ManageBlog';
+
+export const userContext = createContext()
+
+function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
+  return (
+    <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+      <Header></Header>
+        <Switch>
+          <PrivateRoute path="/dashboard">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <PrivateRoute path="/addBlogs">
+            <AddBlog></AddBlog>
+          </PrivateRoute>
+          <PrivateRoute path="/blogManage">
+           <ManageBlog></ManageBlog>
+          </PrivateRoute>
+          <Route path="/login">
+           <Login></Login>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </userContext.Provider>
+    
+  );
+}
+
+export default App;
---
+++ src/Component/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJIHe9cym9t5Ut0ZCmdUkEc4I6yCLh3NA",
+  authDomain: "blog-b15d2.firebaseapp.com",
+  projectId: "blog-b15d2",
+  storageBucket: "blog-b15d2.appspot.com",
+  messagingSenderId: "862944195321",
+  appId: "1:862944195321:web:fa74cf026154d63b24e828"
+};
+  export default firebaseConfig;
